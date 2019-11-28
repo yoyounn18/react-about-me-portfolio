4c0b4e9e8f510b11196c1651a222c84320900202
kim young jo <kimyoungjo@kimui-MacBook-Pro.local>
2019-11-28 14:46:47 +0900

    Updates

---
+++ precache-manifest.50668d505157a51f9eb02bd0f98a6a8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a70149aa20babd027e12371b2839e6c2",
+    "url": "/react-about-me/index.html"
+  },
+  {
+    "revision": "bbf1a60ed0cbad2a08d7",
+    "url": "/react-about-me/static/css/main.89e76373.chunk.css"
+  },
+  {
+    "revision": "4d19eed7aaed904e250f",
+    "url": "/react-about-me/static/js/2.23b5f52a.chunk.js"
+  },
+  {
+    "revision": "bbf1a60ed0cbad2a08d7",
+    "url": "/react-about-me/static/js/main.227b3a77.chunk.js"
+  },
+  {
+    "revision": "9ac1ee1e49a8b0aa2c57",
+    "url": "/react-about-me/static/js/runtime-main.657d49df.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.198c3471e2cf377cf7cce3661a166884.js"
+  "/react-about-me/precache-manifest.50668d505157a51f9eb02bd0f98a6a8d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-about-me/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
