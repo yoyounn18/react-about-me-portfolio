a415810d46c33694c6cfa791e008d18431ea19db
kim young jo <kimyoungjo@kimui-MacBook-Pro.local>
2019-11-28 13:55:21 +0900

    Updates

---
+++ precache-manifest.198c3471e2cf377cf7cce3661a166884.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e4f6d629eb55186b19f8d7b805bfe8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58fdfacec87d51b99778",
+    "url": "/static/css/main.bec84eea.chunk.css"
+  },
+  {
+    "revision": "cd9a3b87b4f8e677b7ae",
+    "url": "/static/js/2.eed77f1b.chunk.js"
+  },
+  {
+    "revision": "58fdfacec87d51b99778",
+    "url": "/static/js/main.eb94cad1.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  }
+]);
